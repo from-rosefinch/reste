c9b843a4d35665fa1ae733e26bebe23360d0ab88
daehyeonkim0326 <daehyeonkim0326@gmail.com>
2025-12-26 17:18:41 +0900

    진짜 찐찐 최종버전입니다!

--- src/components/CartPopup.js
+++ src/components/CartPopup.js
@@ -7,7 +7,8 @@ const CartPopup = ({onClose}) => {
   return (
     <div className="cart-popup-overlay">
       <div className="cart-popup">
-        <h3>장바구니에 추가되었습니다!</h3>
+        <h3 className="name-txt">장바구니에 추가되었습니다!</h3>
+        
           <div className="btn">
             <button onClick={onClose}>더 둘러보기</button>
             <button onClick={() => navigate('/cart')}>장바구니로 이동</button>
